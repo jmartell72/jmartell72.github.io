3aa7fa9a2f595dbb841fc6e9075f7fcf54543114
kingmartell72 <justin.martell13@gmail.com>
2024-10-04 01:11:39 -0400

    Made navbar and footer generate dynamically for each page; Started working on a file for a project to make a template

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -3,28 +3,8 @@ function addHome() {
     div.id = "home";
 
     div.innerHTML = `
-        <nav class="navbar sticky-top navbar-expand-md navbar-dark" style="background-color: #8e7da7 ;">
-        <div class="container-fluid" id="navContainer">
-            <a class="navbar-brand" href="home.html">Justin Martell's Portfolio</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggle"
-                aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarToggle">
-                <ul class="navbar-nav mb-2 mb-lg-0 j flex">
-                    <li class="nav-item"><a class="nav-link" href="./School_Projects.html"
-                            aria-current="School Projects">School Projects</a>
-                    </li>
-                    <li class="nav-item"><a class="nav-link" href=".">Java Projects</a></li>
-                    <li class="nav-item"><a class="nav-link" href=".">HTML Projects</a></li>
-                    <li class="nav-item"><a class="nav-link" href=".">Python Projects</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-        </script>
+    <div id="nav-placeholder"></div>
+
     <main>
         <header>
             <h1>Welcome to Justin Martell's Portfolio</h1>
@@ -121,21 +101,7 @@ function addHome() {
 
         </div>
     </main>
-
-    <footer>
-        <div id="socialMediaIcon">
-            <p>Follow me on LinkedIn!
-                <a href="www.linkedin.com"><img src="./Images/LI-In-Bug.png" alt="LinkedIn Logo">
-                </a>
-            </p>
-        </div>
-        <div id="navBorder">
-        </div>
-        <div id="navLegal">
-            <p>2024© Justin Martell</p>
-        </div>
-
-    </footer>
+    <div id="footer-placeholder"></div>
     `;
     document.getElementById('box1').appendChild(div);
 }
@@ -147,6 +113,14 @@ function removeItem(input) {
 function changeToHome() {
     removeItem(landing);
     addHome();
+    $.get("partials/navbar.html", function (data) {
+
+        $("#nav-placeholder").replaceWith(data);
+    });
+    $.get("partials/footer.html", function (data) {
+
+        $("#footer-placeholder").replaceWith(data);
+    })
 }
 
 // function landingChange() {
